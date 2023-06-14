d5e904ffbdb826c647f7e3bde672819d2a7ea97c
Shao Yujie <syj@hkjt.cn>
2023-06-14 17:28:07 +0800

    字体大小未设置

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -7,20 +7,24 @@
 						2022年12月15日
 					</text>
 				</view>
-				
+
 				<view class="index-item-leftBox-titleBox ">
 					<text class="index-item-leftBox-titleBox-text">
 						项目一项目一项目一项目一项目一项目一项目一项目一项目一
 					</text>
 				</view>
 			</view>
-			
+
 			<view class="index-item-rightBox">
 				<view class="index-item-rightBox-lastDayBox">
-					最后汇报时间:21年8月5日
+					<text class="index-item-rightBox-lastDayBox-text">
+						最后汇报时间:21年8月5日
+					</text>
 				</view>
 				<view class="index-item-rightBox-numBox">
-					10个汇报
+					<text class="index-item-rightBox-numBox-text">
+						10个汇报
+					</text>
 				</view>
 			</view>
 		</view>
@@ -41,14 +45,24 @@
 	}
 </script>
 
-<style>	
-	.list{
+<style>
+	.list {
 		list-style: none;
-		font-size: 20rpx;
 	}
-	
-	.index-item{
+
+	.index-item {
 		display: flex;
 		justify-content: space-between;
+		border: 1rpx solid #CDCDCD;
+	}
+
+	.index-item-leftBox {
+		padding: 10rpx;
+		width: 60%;
+	}
+
+	.index-item-rightBox {
+		padding: 10rpx;
+		width: 40%;
 	}
 </style>
